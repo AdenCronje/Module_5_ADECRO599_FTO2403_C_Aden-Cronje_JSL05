f8359f2cc742082e3de508174ed45d25a3133c59
Aden Cronje <adencronje05@gmail.com>
2024-05-21 11:38:01 +0200

    Added new song genre's

--- index.js
+++ index.js
@@ -11,21 +11,34 @@ const songs = [
     { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
+    { title: "Mrs. Officer", artist: "Lil Wayne", genre: "R&B" },
+    { title: "Lady - (Hear me tonight)", artist: "Modjo", genre: "House" },
+    { title: "Get Lucky", artist: "Daft Punk", genre: "House" },
+    { title: "How Do U Want It", artist: "Tupac", genre: "Hip Hop" },
+    { title: "Music Sounds Better With You", artist: "Stardust", genre: "House" },
+    { title: "Power", artist: "Kanye West", genre: "Hip Hop" },
+    { title: "Redrum", artist: "21 Savage", genre: "Hip Hop" },
     // Feel free to add even more songs
 ];
 
 
 // Object containing each Guardian's preferred genre
 const guardians = {
-    "Star-Lord": "Rock",
-    "Gamora": "Pop",
-    // Add preferences for Drax, Rocket, and Groot
+  "Star-Lord": "Rock",
+  Gamora: "Pop",
+  Drax: "R&B",
+  Rocket: "Hip Hop",
+  Groot: "House",
+  // Add preferences for Drax, Rocket, and Groot
 };
 
 // Function to generate playlist based on preferred genre
 function generatePlaylist(guardians, songs) {
     // Use the map() function to create playlists for each Guardian
     // Your code here
+    guardiansPlaylist = document.getElementById("playlists");
+
+    
 }
 
 // Call generatePlaylist and display the playlists for each Guardian
